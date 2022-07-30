3228ae5fa64ea2fcafccc06b0e9f59d72fc59ad4
Erkinovich07 <107826455+Erkinovich07@users.noreply.github.com>
2022-07-30 12:33:35 +0500

    Add files via upload

---
+++ OOP haqida qisqacha malumot.py
@@ -0,0 +1,92 @@
+#OOP buyicha qisqacha
+
+#Object Oriented Programming
+
+#Obyektlar asosida qurulgan dasturlash uslaubi.OPP ga ko'ra xayotdagi narsalarning dasturimiz ichida obyekt ko'rinishida ta'riflashimiz mumkin.
+
+
+#Object Oriented Programiming yani OOP ning 4ta tayanch ustunlari bor 
+#Bular
+#1 Encapsulation-bu misol uchun avtomabilni olsak bu aftomabilni usti encapsulation deyiladi 
+#chunki aftomabilning usti ichki mexonizimini ko'rsatmaslik vazifasini bajaradi
+#encapsulation xam shunday g'ilof vazifasini utab ichki narsalarni kursatmaydi
+
+
+#2-Polymarfizm bu butta ishni xar xil uslubda bajarish yulga quyiladi 
+#Misol uchun moshina va ot 2lasi xam yuradi faqat boshqacha usulda yana bita misol
+#mashina va ot 2lasini xam ovozi bor faqat moshinada signal ot esa kiwnaydi lekin vazifasi bir xil faqat bowqacha uslubda.
+
+#3-Inheritance-manosi meros olish
+#bunda 1ta class asosida yangi class yarata olamiz va uwa yangi classda boyegi class degi xama xususyatlarni buladi.Bundan tawqari
+# uwa yangi classda yangi xususyatlar e'lon qiliwimiz mumkin.
+
+
+#Abstract class - avlod klaslar meros oladigan hamda hohlasa o'zgartirishimi mumkin bo'lgan xossa va metodlarni o'zida jamlaydi.
+#Unda metod va xossalarni "Implementatsiya" ham bo'lish mumkin.
+#Abstract klass yordamida obyekt tuzib bo'lmaydi yani "Instantiate" qilib bo'lmaydi
+#Abstract mavxum class deyiladi
+
+
+#Class lar
+#class da abyekt tuzuwimiz mumkin Abstract claass'da aniqlangan metod va hossalarni kerak bo'lsa 
+# "override" qilish mumkin yani bekor qilishi mumkin
+
+
+#Interface
+
+#Interface - klass tomonidan qo'llanilishi mumkin bo'lgan meto d va xossalarni ta'riflaydi xolos.
+# Interfeysda metod va xossalarni Implementatsiyasi bo'lmaydi.
+
+
+#Object
+
+#Object bu o'zgaruvchi  class asosida qurulgan konkrekt prayekt
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
